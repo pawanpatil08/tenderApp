a3ca1a476803d3399cc38bf0b702cc3f84c41bfe
Shrinivas Nawgire <shrinivasnawgire@gmail.com>
2020-12-15 20:55:05 +0530

    Adde signup page

--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  register(form) {
+    console.log(form);
+    // this.authService.register(form.value).subscribe((res) => {
+    //   this.router.navigateByUrl('home');
+    // });
   }
 
 }
