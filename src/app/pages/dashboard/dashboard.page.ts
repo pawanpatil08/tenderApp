1bc03cb2a7d8f7c52809e6ab2b428f40197d9057
Pawan Patil <pwnpatil08@gmail.com>
2020-12-17 19:39:19 +0530

    dashboard changes

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from "@angular/fire/database";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  tenderList = ['Building Construction', 'Chemicals', 'Civil Works', 'Computer Equipment & Accessories', 'Furniture',
+    'Laboratory & scientific equipment', 'Road Construction', 'Scrap & waste materials',
+    'Software',
+    'Transportation Services'];
 
-  constructor() { }
+  constructor( private afDb: AngularFireDatabase) { }
 
   ngOnInit() {
+    // this.afDb.database.
+    // this.afDb.list('tenderList').push(['Building Construction', 'Chemicals', 'Civil Works', 'Computer Equipment & Accessories', 'Furniture',
+    // 'Laboratory & scientific equipment', 'Road Construction', 'Scrap & waste materials',
+    // 'Software',
+    // 'Transportation Services']);
+      
+  
   }
 
 }
