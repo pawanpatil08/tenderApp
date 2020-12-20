a9664965b027a1acda4d6d9c170002f9edb42396
Shrinivas Nawgire <shrinivasnawgire@gmail.com>
2020-12-20 11:42:59 +0530

    Removed unwanted options from home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,31 +16,6 @@ export class AppComponent implements OnInit {
       title: 'Login',
       url: '/login',
       icon: 'log-in'
-    },
-    {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
-    },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
     }
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
